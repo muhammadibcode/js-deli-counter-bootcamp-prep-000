2338e89d0a52b2e082df4099fe026c4478ac2d35
Muhammad Bhatti <muhammad.i.b@outlook.com>
2019-08-03 16:34:46 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,24 @@
+
+function takeANumber(queue, newperson) {
+  queue.push(newperson);
+  return `Welcome, ${newperson}. You are number ${queue.length + 1} in line.`;
+}
+
+function nowServing(queue) {
+  if (queue.length === 0) {
+    return "There is nobody waiting to be served!";
+  } else {
+    var firstperson = queue[0];
+    queue.shift();
+    return firstperson;
+         }
+
+}
+
+function currentLine(queue) {
+  if (queue.length === 0) {
+    return "The line is currently empty.";
+  } else {
+    
+  }
+}

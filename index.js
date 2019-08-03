1292ea8273a8d09fdd5aab1f75d3972b222e83be
Muhammad Bhatti <muhammad.i.b@outlook.com>
2019-08-03 17:09:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,7 +29,8 @@ function currentLine(queue) {
     // If the queue length is 3, 'i' will increment and execute the for loop until it reaches 3.  On 3 it will not execute the loop.
     for (var i = 0; i != queue.length; i++) {
     // 'i' will be the index of each person in the queue.  'i + 1' is the actual position of a person in the queue.
-      message = message + `${i + 1}. ${queue[i]}` + if (i != queue.length - 1) {','}
+      message = message + `${i + 1}. ${queue[i]}`;
+      if (i != queue.length - 1) {+ ','};
     }
     return message;
   }

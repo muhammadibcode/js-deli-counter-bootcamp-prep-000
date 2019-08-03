f8d34a3810a47c071ae66934af49bbf9ae7d4929
Muhammad Bhatti <muhammad.i.b@outlook.com>
2019-08-03 16:43:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-// 
+// Deli Counter Lab
+
+// Take A Number
 
 function takeANumber(queue, newperson) {
   queue.push(newperson);
   return `Welcome, ${newperson}. You are number ${queue.length + 1} in line.`;
 }
+
+// Now Serving
 
 function nowServing(queue) {
   if (queue.length === 0) {
@@ -13,15 +17,16 @@ function nowServing(queue) {
     queue.shift();
     return firstperson;
          }
-
 }
+
+// CurrentLine 
 
 function currentLine(queue) {
   if (queue.length === 0) {
     return "The line is currently empty.";
   } else {
     numberofpeople = queue.length;
-    return `The line is currently: 1 $`
+    return `The line is currently:` + 
     for (i = 0, i = numberofpeople, numberofpeople--) {
 
     }
